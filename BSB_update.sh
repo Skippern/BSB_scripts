3a1ae07d830f1ee67cf7a3a5f0244226f50f62c5
Nelson A. de Oliveira <naoliv@debian.org>
2014-07-08 16:21:45 -0300

    Adding a cache for downloaded files

--- BSB_update.sh
+++ BSB_update.sh
@@ -2,28 +2,30 @@
 
 set -e
 
-echo "Start downloading ODBL compatible RNC maps of the world"
+echo "Downloading ODBL compatible RNC maps of the world"
 
 TMPDIR=`mktemp -d`
-BSBDIR='/Users/skippern/Documents/Maps/BSB/'
+BSBDIR='~/maps/BSB/'
+CACHEDIR='~/maps/cache'
 
-cd $TMPDIR
+mkdir -p $BSBDIR/{BR,AR,US,NZ} $CACHEDIR
 
+trap 'rm -rf "$TMPDIR"' 0
+trap "exit 2" 1 2 3 15
 
 # Brazil (BR) is of compatible license (PD) http://www.mar.mil.br/dhn/chm/box-cartas-raster/raster_disponiveis.html
 
 wget -q http://www.mar.mil.br/dhn/chm/box-cartas-raster/raster_disponiveis.html -O - \
-	| sed -n 's/.*<a href="cartas\/\(.*.zip\)">.*/\1/p' > files.txt
+	| sed -n 's/.*<a href="cartas\/\(.*.zip\)">.*/\1/p' > $TMPDIR/files.txt
 
-for i in `cat files.txt`; do
+for i in `cat $TMPDIR/files.txt`; do
 	echo "Working with BR: $i"
-	wget -q http://www.mar.mil.br/dhn/chm/box-cartas-raster/cartas/$i
-	if [ -s $i ]; then
-		unzip -qjoC $i \*.BSB \*.KAP -d $BSBDIR/BR
-		rm $i
+	wget -Nq http://www.mar.mil.br/dhn/chm/box-cartas-raster/cartas/$i -P $CACHEDIR
+	if [ -s $CACHEDIR/$i ]; then
+		unzip -qjoC $CACHEDIR/$i \*.BSB \*.KAP -d $BSBDIR/BR
 	fi
 done
-rm files.txt
+rm $TMPDIR/files.txt
 
 # Argentina (AR) is of compatible license (PD) http://www.hidro.gob.ar/cartas/cartasnauticas.asp
 
@@ -31,38 +33,35 @@ for i in `wget -q http://www.hidro.gob.ar/cartas/cartasnauticas.asp -O - \
 	| sed -n 's/.*<a href="\(CNRaster\.asp?r=.*\)">.*/\1/p'`; do
 
 	wget -q http://www.hidro.gob.ar/cartas/$i -O - \
-		| sed -n 's/.*<a href="BSB\/\(.*.zip\)">.*/\1/p' >> files.txt
+		| sed -n 's/.*<a href="BSB\/\(.*.zip\)">.*/\1/p' >> $TMPDIR/files.txt
 done
 
-for i in `cat files.txt`; do
+for i in `cat $TMPDIR/files.txt`; do
 	echo "Working with AR: $i"
-	wget -q http://www.hidro.gob.ar/cartas/BSB/$i
-	if [ -s $i ]; then
+	wget -Nq http://www.hidro.gob.ar/cartas/BSB/$i -P $CACHEDIR
+	if [ -s $CACHEDIR/$i ]; then
 		# the files are actually RAR
-		unrar e -ep -o+ -inul $i \*.BSB \*.KAP $BSBDIR/AR
-		rm $i
+		unrar e -ep -o+ -inul $CACHEDIR/$i \*.BSB \*.KAP $BSBDIR/AR
 	fi
 done
-rm files.txt
+rm $TMPDIR/files.txt
 
 # USA (US) is of compatible license (PD) http://www.charts.noaa.gov/RNCs
 
 # ~2.9G
-wget -q http://www.charts.noaa.gov/RNCs/All_RNCs.zip
-if [ -s $i All_RNCs.zip]; then
+wget -Nq http://www.charts.noaa.gov/RNCs/All_RNCs.zip -P $CACHEDIR
+if [ -s $CACHEDIR/All_RNCs.zip]; then
 	echo "Working with US: All_RNCs.zip"
-	unzip -qjoC $i \*.BSB \*.KAP -d $BSBDIR/US
-	rm All_RNCs.zip
+	unzip -qjoC $CACHEDIR/$i \*.BSB \*.KAP -d $BSBDIR/US
 fi
 
 # New Zealand (NZ) is of compatible license (CC-BY) http://www.linz.govt.nz/hydro/charts/digital-charts/nzmariner
 
 for i in BSB_BASE.zip bsb_update.zip; do
 	echo "Working with NZ: $i"
-	wget -q http://www.linz.govt.nz/sites/default/files/docs/hydro/charts/digital/nzmariner/$i
-	if [ -s $i ]; then
-		unzip -qjoC $i \*.BSB \*.KAP -d $BSBDIR/NZ
-		rm $i
+	wget -Nq http://www.linz.govt.nz/sites/default/files/docs/hydro/charts/digital/nzmariner/$i -P $CACHEDIR
+	if [ -s $CACHEDIR/$i ]; then
+		unzip -qjoC $CACHEDIR/$i \*.BSB \*.KAP -d $BSBDIR/NZ
 	fi
 done
 
@@ -77,7 +76,5 @@ done
 # Venezuela (VE) http://www.dhn.mil.ve are commercial
 
 # See http://rossi.christian.free.fr/nautical_free.html for more
-
-rm -rf $TMPDIR
 
 echo "Done!"
