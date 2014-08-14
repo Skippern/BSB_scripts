a249a203ef96f9d3e191f2b7c3cce8064be44915
Nelson A. de Oliveira <naoliv@debian.org>
2014-08-14 08:13:01 -0300

    Fix #1: make mktemp portable

--- BSB_update.sh
+++ BSB_update.sh
@@ -4,11 +4,13 @@ set -e
 
 echo "Downloading ODBL compatible RNC maps of the world"
 
-TMPDIR=`mktemp -d`
+TMP=`mktemp -d ${TMPDIR:-/tmp}/tmp.XXXXXXXXXX`
 # BSBDIR needs +6.7G
 BSBDIR='~/maps/BSB'
 # CACHEDIR needs +3.4G
 CACHEDIR='~/maps/cache'
+
+# TODO: needs to clean old and unused files under CACHEDIR
 
 mkdir -p $BSBDIR/BR \
 	$BSBDIR/AR \
@@ -16,22 +18,22 @@ mkdir -p $BSBDIR/BR \
 	$BSBDIR/NZ \
 	$CACHEDIR
 
-trap 'rm -rf "$TMPDIR"' 0
+trap 'rm -rf "$TMP"' 0
 trap "exit 2" 1 2 3 15
 
 # Brazil (BR) is of compatible license (PD) http://www.mar.mil.br/dhn/chm/box-cartas-raster/raster_disponiveis.html
 
 wget -q http://www.mar.mil.br/dhn/chm/box-cartas-raster/raster_disponiveis.html -O - \
-	| sed -n 's/.*<a href="cartas\/\(.*.zip\)">.*/\1/p' > $TMPDIR/files.txt
+	| sed -n 's/.*<a href="cartas\/\(.*.zip\)">.*/\1/p' > $TMP/files.txt
 
-for i in `cat $TMPDIR/files.txt`; do
+for i in `cat $TMP/files.txt`; do
 	echo "Working with BR: $i"
 	wget -Nq http://www.mar.mil.br/dhn/chm/box-cartas-raster/cartas/$i -P $CACHEDIR || true
 	if [ -s $CACHEDIR/$i ]; then
 		unzip -qjoC $CACHEDIR/$i \*.BSB \*.KAP -d $BSBDIR/BR
 	fi
 done
-rm $TMPDIR/files.txt
+rm $TMP/files.txt
 
 # Argentina (AR) is of compatible license (PD) http://www.hidro.gob.ar/cartas/cartasnauticas.asp
 
@@ -39,10 +41,10 @@ for i in `wget -q http://www.hidro.gob.ar/cartas/cartasnauticas.asp -O - \
 	| sed -n 's/.*<a href="\(CNRaster\.asp?r=.*\)">.*/\1/p'`; do
 
 	wget -q http://www.hidro.gob.ar/cartas/$i -O - \
-		| sed -n 's/.*<a href="BSB\/\(.*.zip\)">.*/\1/p' >> $TMPDIR/files.txt
+		| sed -n 's/.*<a href="BSB\/\(.*.zip\)">.*/\1/p' >> $TMP/files.txt
 done
 
-for i in `cat $TMPDIR/files.txt`; do
+for i in `cat $TMP/files.txt`; do
 	echo "Working with AR: $i"
 	wget -Nq http://www.hidro.gob.ar/cartas/BSB/$i -P $CACHEDIR
 	if [ -s $CACHEDIR/$i ]; then
@@ -56,7 +58,7 @@ for i in `cat $TMPDIR/files.txt`; do
 		fi
 	fi
 done
-rm $TMPDIR/files.txt
+rm $TMP/files.txt
 
 # USA (US) is of compatible license (PD) http://www.charts.noaa.gov/RNCs
 
